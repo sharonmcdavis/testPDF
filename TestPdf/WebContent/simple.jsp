0fb38def842fa3cf86157bc2b6986d6f1b3e3b56
sd991t <sd991t@TXCDTL11RT7256.ITServices.sbc.com>
2018-08-23 15:53:21 -0500

    initial commit

---
+++ TestPdf/WebContent/simple.jsp
@@ -0,0 +1,17 @@
+pre-report
+
+
+<script src="../Javascript/report.js"></script>
+
+<FORM name="reportForm" method="post" action="./control/ReportController" target="_self">
+<INPUT type="hidden" name="methodName" value="getDepletionReport">
+<input type="hidden" name="pageId" value="">
+<input type="hidden" name="referer" value="">
+<input type="hidden" name="refererTitle" value="">
+
+<!--  don't need validation for testing 
+<INPUT type="submit" name="View Report" value="View Report" onclick="return validate()">
+ -->
+ <INPUT type="submit" name="View Report" value="View Report">
+
+</form>
---
+++ TestPdf/src/com/control/ReportController.java
@@ -0,0 +1,85 @@
+package com.control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ReportController extends HttpServlet {
+
+	public void getDepletionReport(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException
+	{
+		try
+		{
+			HttpSession session = req.getSession();
+			//ReportUtils.getDepletionReport(paramArr, resp, session,	rptBean, req);
+			this.forward(request, response, "/WEB-INF/report1.jsp");
+		}
+		catch (Exception ex)
+		{
+			this.errorPage(req, resp, ex.getMessage());
+		}
+	}
+	//end getDepletionReport
+
+	protected void forward(HttpServletRequest req, HttpServletResponse res, String pageName)
+	{
+	    RequestDispatcher rd = req.getRequestDispatcher(pageName);
+	    try
+	    {
+      		rd.forward(req,res);
+    	}
+    	catch (IOException ioex)
+    	{
+      		ioex.printStackTrace();
+    		errorPage(req, res,"An Error has occurred: " + ioex.getMessage());
+    	}
+    	catch(Exception ex)
+    	{
+			//do nothing for a broken pipe exception
+			if (!ex.getMessage().equalsIgnoreCase("Broken pipe") )
+			{
+      		ex.printStackTrace();
+    		errorPage(req, res,"An Error has occurred: " + ex.getMessage());
+			}
+    	}
+	}
+	//end forward
+
+	public void errorPage(HttpServletRequest req, HttpServletResponse res, String message)
+	{
+   		HttpSession session = req.getSession();	
+   		if (message != null)
+   		{
+			System.out.println("error = " + message);
+			session.setAttribute("errorMsg", message);
+			session.setAttribute("msgColor", "red");
+   		}
+   		else
+   		{
+	   		System.out.println("common error page = message is NULL");
+//			session.setAttribute("errorMsg", Utils.getErrorMsg("NullErrorMsg"));
+			session.setAttribute("msgColor", "red");
+   		}
+		RequestDispatcher rd = req.getRequestDispatcher("/common/Errors.jsp");
+		try
+		{
+        	rd.forward(req,res);
+      	}
+      	catch (Exception ex)
+      	{
+			//do nothing for a broken pipe exception
+			if (!ex.getMessage().equalsIgnoreCase("Broken pipe") )
+			{
+        	System.out.println("caught exception on forward");
+        	ex.printStackTrace();
+			}
+      	}
+   	}
+	// end errorPage
+
+}
